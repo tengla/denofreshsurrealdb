e8f8b4cbbc90809a96d62d7df369fc2ad4947b0e
Thomas Eng <thomas.tinnesand.eng@gmail.com>
2022-09-26 13:44:33 +0200

    chore: add a few deps
    SurrealDB, TypeBox and Ajv to be precise.

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,8 +5,10 @@
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/_middleware.ts";
-import * as $2 from "./routes/api/joke.ts";
-import * as $3 from "./routes/index.tsx";
+import * as $2 from "./routes/api/jokes.ts";
+import * as $3 from "./routes/api/jokes/[id].ts";
+import * as $4 from "./routes/api/jokes/random.ts";
+import * as $5 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 import * as $$1 from "./islands/Jokes.tsx";
 
@@ -14,8 +16,10 @@ const manifest = {
   routes: {
     "./routes/[name].tsx": $0,
     "./routes/_middleware.ts": $1,
-    "./routes/api/joke.ts": $2,
-    "./routes/index.tsx": $3,
+    "./routes/api/jokes.ts": $2,
+    "./routes/api/jokes/[id].ts": $3,
+    "./routes/api/jokes/random.ts": $4,
+    "./routes/index.tsx": $5,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
